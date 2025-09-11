8f8919ad13ac083f730d70599396596b5677be4e
echoshade800 <echoshade800@gmail.com>
2025-09-11 14:01:06 +0800

    Fix duplicate route configuration in info layout

--- app/info/_layout.tsx
+++ app/info/_layout.tsx
@@ -5,7 +5,6 @@ export default function InfoLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="cycle-length" />
       <Stack.Screen name="cycle-variation" />
-      <Stack.Screen name="cycle-variation" />
     </Stack>
   );
 }
